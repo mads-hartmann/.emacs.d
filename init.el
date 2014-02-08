2508b35cb2a1effa1f536397c1b7e07e1633185b
Eva Paus Regnar <evapausregnar@Evas-MacBook-Air-5.local>
2014-02-08 23:12:50 +0100

    Use window-numbering-mode

--- init.el
+++ init.el
@@ -33,7 +33,8 @@
    erlang
    ido-vertical-mode
    ido-ubiquitous
-   powerline))
+   powerline
+   window-numbering-mode))
 
 (setq custom-safe-themes '("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
 
