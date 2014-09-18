353a2dfbe96473d58067f128f258d15a678a16e5
Mads Hartmann Jensen <mads379@gmail.com>
2014-09-18 18:25:33 +0200

    Found a nice theme donderom/jazz-theme
    Changed a few colors for helm though.

--- config.el
+++ config.el
@@ -28,6 +28,9 @@
 (custom-set-variables
  '(speedbar-show-unknown-files t))
 
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'jazz t)
+
 (setq custom-safe-themes
       '("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879"
         "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
