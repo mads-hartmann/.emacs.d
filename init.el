51bac97bfeea752a3b1f88d4c2ede1c914ea73db
Mads Hartmann Jensen <mads379@gmail.com>
2016-08-12 14:10:49 +0200

    Don't use marmalade

--- init.el
+++ init.el
@@ -10,7 +10,6 @@
 
 (setq package-enable-at-startup nil)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 ;; Load my packages
